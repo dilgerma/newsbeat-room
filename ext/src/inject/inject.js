03ecd9219439bacebd78110b4d4caa10db2f3f53
Martin <martin@effectivetrainings.de>
2020-03-20 23:37:02 +0100

    voice volume

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -16,7 +16,7 @@ function initializeInterval() {
 
 function script() {
  
- // VERSION 1.6.1
+ // VERSION 1.6.2
 /*
 IF YOU ARE USING THIS SCRIPT AND MAKING MONEY WITH IT.
 PLEASE CONSIDER GIVING SOMETHING BACK - I KINDLY ASK YOU TO DONATE 5 or 10$ TO 
@@ -95,6 +95,7 @@ const fieldShowTradeLogButton = "fieldShowTradelogButton";
 const fieldShowQuickTradesButton = "fieldShowQuickTradesButton";
 const fieldCallQuickTrades = "fieldCallQuickTrades";
 const fieldStopTalking = "fieldStopTalking";
+const fieldIdVolume = "fieldIdVolume";
 
 
 const msgIdAttribute = "msg-id-attribute";
@@ -192,7 +193,7 @@ function _newMessageToSpeak(text) {
   var utterance = new SpeechSynthesisUtterance();
   utterance.text = text;
   utterance.lang = 'en-US';
-  utterance.volume = 1; // 0 to 1
+  utterance.volume = state[fieldIdVolume] ? state[fieldIdVolume]/10 : 1; // 0 to 1
   utterance.rate = 1; // 0.1 to 10
   utterance.pitch = 1; //0 to 2
   return utterance;
@@ -421,7 +422,8 @@ const readState = () => {
     [fieldKeywordsToRead]: [],
     [fieldHideTray] : false,
     [fieldDate] : highlightedMessages,
-    [fieldCallQuickTrades] : false
+    [fieldCallQuickTrades] : false,
+    [fieldIdVolume] : 3
   };
 
   if(state[fieldDate] && state[fieldDate].date != today().getTime()) {
@@ -948,7 +950,7 @@ function prepareSupportAndResistanceWindow() {
 
   const headerTemplateString = `
     <div class="inplay-presenter-header" style="padding: 16px; font-weight: bold; box-sizing: border-box; position: relative; white-space: nowrap; height: 48px; color: rgb(0, 90, 132); background-color: rgb(222, 222, 222);"><div style="display: inline-block; vertical-align: top; white-space: normal; padding-right: 90px;"><span style="color: rgb(0, 0, 0); display: block; font-size: 15px">
-    <a href="https://github.com/dilgerma/newsbeat-room" target="_blank">NewsBeat Script</a>  1.6.1 (unofficial)</span>
+    <a href="https://github.com/dilgerma/newsbeat-room" target="_blank">NewsBeat Script</a>  1.6.2 (unofficial)</span>
     <span style="color: rgba(0, 0, 0, 0.54); display: block; font-size: 14px;"></span>
     </div>
     <button id="${fieldHideForm}" tabindex="0" type="button" style="border: 10px; box-sizing: border-box; display: inline-block; font-family: Roboto, sans-serif; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); cursor: pointer; text-decoration: none; margin: auto; padding: 12px; outline: none; font-size: 0px; font-weight: inherit; position: absolute; overflow: visible; transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms; width: 48px; height: 48px; top: 0px; bottom: 0px; right: 4px; background: none;"><div><svg viewBox="0 0 24 24" style="display: inline-block; color: rgb(0, 0, 0); fill: currentcolor; height: 24px; width: 24px; user-select: none; transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;"><path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"></path></svg></div></button></div>`;
@@ -1025,6 +1027,7 @@ function prepareSupportAndResistanceWindow() {
                                                                     <div>
                                                                     Read Chat: <input id="${fieldSpeakHighlights}" type="checkbox">
                                                                     Read All: <input id="${fieldReadAllEntries}" type="checkbox">
+                                                                    Voice Volume: <input id="${fieldIdVolume}" type="number" min=0 max=10>
                                                                     </div>
                                                                     <div>
                                                                       Keywords to read <input style="width:100%" id="${fieldKeywordsToRead}" type="text" placeholder="Keywords or Names to exclusively read - like TSLA,resistance,support">
@@ -1083,6 +1086,7 @@ document.getElementById(fieldCallQuickTrades).checked = state[fieldCallQuickTrad
 document.getElementById(fieldKeywordsToRead).value = state[fieldKeywordsToRead] && state[fieldKeywordsToRead].length > 0 ? state[fieldKeywordsToRead].reduce(
   (acc, highlighted) => `${acc},${highlighted}`
 ) : "";
+document.getElementById(fieldIdVolume).value = state[fieldIdVolume];
 
 document.getElementById(fieldCollections).value = state[fieldCollections].length > 0 ? state[fieldCollections].reduce(
     (acc, highlighted) => `${acc},${highlighted}`
@@ -1186,6 +1190,11 @@ document.getElementById(fieldSpeakHighlights).addEventListener("change", (evt)=>
 });
 document.getElementById(fieldKeywordsToRead).addEventListener("change", (evt)=>{
   state[fieldKeywordsToRead] = evt.target.value.split(",").map(entry => entry.trim()).filter(entry => entry.length > 0);
+  storeState(state);
+});
+
+document.getElementById(fieldIdVolume).addEventListener("change", (evt) => {
+  state[fieldIdVolume] = evt.target.value;
   storeState(state);
 });
 
