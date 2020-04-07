608a401fe76135d0d287ff0e7e672712c98db0d2
Martin <martin@effectivetrainings.de>
2020-04-07 22:48:29 +0200

    1.8.1

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -18,7 +18,7 @@ function script() {
  
   
   
- // VERSION 1.8.0
+ // VERSION 1.8.1
 /*
 IF YOU ARE USING THIS SCRIPT AND MAKING MONEY WITH IT.
 PLEASE CONSIDER GIVING SOMETHING BACK - I KINDLY ASK YOU TO DONATE 5 or 10$ TO 
@@ -154,6 +154,7 @@ const highlightTextColor = "black";
 const collection = {};
 var selectedCollection = ["None"];
 const staticCollections = [];
+const markedMessagesKey = "Marked Messages";
 
 //highlighted messages 
 const today = () => new Date(new Date().getFullYear(),new Date().getMonth() , new Date().getDate());
@@ -393,10 +394,14 @@ const findAllMessages = (filterProcessed) => {
 }
 
 //marks a message via dbl click
-const markMessage = (node) => node.setAttribute(
-  "style",
-  `background-color:${getMarkerColor()};color:${markerTextColor}`
-);
+const markMessage = (node) => {
+    const msg = parseEntry(node);
+    handleCollection(markedMessagesKey, msg);
+    node.setAttribute(
+      "style",
+      `background-color:${getMarkerColor()};color:${markerTextColor}`
+    );
+  }
 
 const sortByDate = (collection) => {
   return collection.sort((first, second) => {
@@ -781,20 +786,22 @@ const updateCollecions = selection => {
   //remove all nodes, sorted ones will be reapplied
   removeAllElementsInNode(container);
 
-  collection[selection].forEach(msg => {
-    const node = document.createElement("div");
-    node.setAttribute("processed", "true");
-    const msgTemplate = `
-                                                                      <li class="chat-message" processed="true">
-                                                                        <span class="chat-message-timestamp">${msg.date}</span>
-                                                                        <span class="chat-message-username">${msg.name}</span>
-                                                                        <span class="chat-message-username">:</span>
-                                                                        <span class="chat-message-text">${msg.text}</span>
-                                                                      </li>`;
-    node.innerHTML = msgTemplate;
+  if(collection[selection]) {
+    collection[selection].forEach(msg => {
+      const node = document.createElement("div");
+      node.setAttribute("processed", "true");
+      const msgTemplate = `
+                                                                        <li class="chat-message" processed="true">
+                                                                          <span class="chat-message-timestamp">${msg.date}</span>
+                                                                          <span class="chat-message-username">${msg.name}</span>
+                                                                          <span class="chat-message-username">:</span>
+                                                                          <span class="chat-message-text">${msg.text}</span>
+                                                                        </li>`;
+      node.innerHTML = msgTemplate;
 
-    container.appendChild(node);
-  });
+      container.appendChild(node);
+    });
+  }
 };
 
 
@@ -966,7 +973,7 @@ function prepareSupportAndResistanceWindow() {
 
   const headerTemplateString = `
     <div class="inplay-presenter-header" style="padding: 16px; font-weight: bold; box-sizing: border-box; position: relative; white-space: nowrap; height: 48px; color: rgb(0, 90, 132); background-color: rgb(222, 222, 222);"><div style="display: inline-block; vertical-align: top; white-space: normal; padding-right: 90px;"><span style="color: rgb(0, 0, 0); display: block; font-size: 15px">
-    <a href="https://github.com/dilgerma/newsbeat-room" target="_blank">NewsBeat Script</a>  1.8.0 (unofficial)</span>
+    <a href="https://github.com/dilgerma/newsbeat-room" target="_blank">NewsBeat Script</a>  1.8.1 (unofficial)</span>
     <span style="color: rgba(0, 0, 0, 0.54); display: block; font-size: 14px;"></span>
     </div>
     <button id="${fieldHideForm}" tabindex="0" type="button" style="border: 10px; box-sizing: border-box; display: inline-block; font-family: Roboto, sans-serif; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); cursor: pointer; text-decoration: none; margin: auto; padding: 12px; outline: none; font-size: 0px; font-weight: inherit; position: absolute; overflow: visible; transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms; width: 48px; height: 48px; top: 0px; bottom: 0px; right: 4px; background: none;"><div><svg viewBox="0 0 24 24" style="display: inline-block; color: rgb(0, 0, 0); fill: currentcolor; height: 24px; width: 24px; user-select: none; transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;"><path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"></path></svg></div></button></div>`;
